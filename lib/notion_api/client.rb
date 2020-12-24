6db4ec12d910e6ae041a233032379bbfb3c1421f
dan murphy <danielmurph8@gmail.com>
2020-12-23 19:54:14 -0500

    remove debugging text

--- lib/notion_api/client.rb
+++ lib/notion_api/client.rb
@@ -15,11 +15,3 @@ module NotionAPI
     end
   end
 end
-
-@client = NotionAPI::Client.new(ENV["token_v2"])
-@page = @client.get_page("https://www.notion.so/danmurphy/d9d49ca64a2a4911ad6dffe898a468e5?v=28d7c1ea5fe347d8ae002eee11e10888")
-p @page
-# @block = @page.create(NotionAPI::TextBlock, "Title")
-# @block.title="NEW"
-# @img = @page.create(NotionAPI::ImageBlock, "https://images.unsplash.com/photo-1467348733814-f93fc480bec6?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb", options: { image: "https://images.unsplash.com/photo-1467348733814-f93fc480bec6?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb" })
-# p @img.title
